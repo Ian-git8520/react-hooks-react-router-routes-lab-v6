3ac232bf318c56d3f303f896db7c6885e0f3ed07
ian_git8520 <iandavidnjenga@gmail.com>
2025-10-19 21:20:05 +0300

    initial commit

---
+++ src/components/Actors.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar.jsx";
+
+function Actors() {
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then((response) => response.json())
+      .then((data) => setActors(data));
+  }, []);
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <article key={actor.id}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
+      </main>
+    </>
+  );
+}
+
+export default Actors;
---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar.jsx";
+import MovieCard from "../components/MovieCard.jsx";
+
+function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Home Page</h1>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </main>
+    </>
+  );
+}
+
+export default Home;
